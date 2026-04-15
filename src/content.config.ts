e7541f323cfbabdf1704b3aaaa81ac85491b1165
Xiuyou01 <xiuyou01@users.noreply.github.com>
2026-04-15 18:00:33 +0800

    initial commit: deploy setup

---
+++ src/content.config.ts
@@ -0,0 +1,16 @@
+import { z, defineCollection } from 'astro:content';
+import { glob } from 'astro/loaders';
+
+const thoughtsCollection = defineCollection({
+  loader: glob({ pattern: "**/*.md", base: "./src/content/thoughts" }),
+  schema: z.object({
+    title: z.string(),
+    date: z.date(),
+    excerpt: z.string(),
+    tags: z.array(z.string()).optional(),
+  }),
+});
+
+export const collections = {
+  'thoughts': thoughtsCollection,
+};
